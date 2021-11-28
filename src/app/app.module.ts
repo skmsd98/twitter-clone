f8aced9ac0111c292cb4c52e67e4586edd7d5d09
skmsd98 <skmsd98@gmail.com>
2021-11-28 13:17:32 +0500

    created three components, added html markup for sidebar-header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarHeaderComponent } from './sidebar-header/sidebar-header.component';
+import { SidebarOthersComponent } from './sidebar-others/sidebar-others.component';
+import { ContentAreaComponent } from './content-area/content-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarHeaderComponent,
+    SidebarOthersComponent,
+    ContentAreaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sidebar-others/sidebar-others.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sidebar-others',
+  templateUrl: './sidebar-others.component.html',
+  styleUrls: ['./sidebar-others.component.scss']
+})
+export class SidebarOthersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
